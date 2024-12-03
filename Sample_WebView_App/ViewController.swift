cfacc2c265dc86b5e8a24378a049e46df5aac95d
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-03 21:19:48 +0900

    Initial Commit

---
+++ Sample_WebView_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample_WebView_App
+//
+//  Created by ウルトラ深瀬 on 3/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
